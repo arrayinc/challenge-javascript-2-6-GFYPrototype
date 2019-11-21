ff166c878a8d6a99871c637e911c1a0da0c17d5c
jwelchman <j.welchman@yahoo.com>
2019-11-21 10:09:06 -0700

    finished final challenge. HURRAH

--- answer.js
+++ answer.js
@@ -1,1 +1,11 @@
 // *** YOUR ANSWER BELOW ***
+
+let years = 1;
+
+function yearsToMonthsToDays(years) {
+    let months = years * 12;
+    let days = months * 365;
+    return years + " " + "year(s)" + " is equal to " + Math.floor(months) + " " + "month(s)" + " or " + days + " days.";
+}
+
+console.log(yearsToMonthsToDays(12));
